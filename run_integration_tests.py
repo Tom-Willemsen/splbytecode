962bdafbdaea92b6815878583558f587dc7a55bd
Tom Willemsen <tomwillemsen1995@gmail.com>
2017-12-03 12:14:03 +0000

    Add primality test to examples

--- run_integration_tests.py
+++ run_integration_tests.py
@@ -4,6 +4,8 @@ import os
 
 import shutil
 
+from math import sqrt
+
 import splbytecode
 import subprocess
 
@@ -42,6 +44,12 @@ def run_java(class_name, *args):
     return output.decode()
 
 
+def remove_junk_line(text):
+    if text.split(os.linesep)[0].startswith("Picked up _JAVA_OPTIONS:"):
+        return os.linesep.join(text.split(os.linesep)[1:])
+    return text
+
+
 def remove_cr_and_lf(text):
     return "".join(c for c in text if c not in ["\r", "\n"])
 
@@ -58,7 +66,7 @@ class IntegrationTests(unittest.TestCase):
 
         compile_spl(filename, class_name)
 
-        output = remove_cr_and_lf(run_java(class_name))
+        output = remove_cr_and_lf(remove_junk_line(run_java(class_name)))
 
         self.assertIn("HELLO, WORLD", output)
 
@@ -68,7 +76,7 @@ class IntegrationTests(unittest.TestCase):
 
         compile_spl(filename, class_name)
 
-        output = remove_cr_and_lf(run_java(class_name, 12321))
+        output = remove_cr_and_lf(remove_junk_line(run_java(class_name, 12321)))
 
         self.assertIn("12322", output)
 
@@ -78,9 +86,9 @@ class IntegrationTests(unittest.TestCase):
 
         compile_spl(filename, class_name)
 
-        output = remove_cr_and_lf(run_java(class_name))
+        output = remove_junk_line(run_java(class_name))
 
-        self.assertIn("1", output)
+        self.assertEqual("1" + os.linesep, output)
 
     def test_GIVEN_conditional_goto_example_THEN_it_compiles_and_runs_without_error(self):
         filename = "condgoto.spl"
@@ -88,11 +96,31 @@ class IntegrationTests(unittest.TestCase):
 
         compile_spl(filename, class_name)
 
-        output = remove_cr_and_lf(run_java(class_name, 15))
+        output = remove_junk_line(run_java(class_name, 15))
 
-        expected_output = "".join(str(n) for n in range(1, 16))
+        expected_output = os.linesep.join(str(n) for n in range(1, 16)) + os.linesep
 
-        self.assertIn(expected_output, output)
+        self.assertEqual(expected_output, output)
+
+    def test_GIVEN_prime_test_example_THEN_it_compiles_and_runs_without_error(self):
+        filename = "prime.spl"
+        class_name = class_name_from_filename(filename)
+
+        compile_spl(filename, class_name)
+
+        def is_prime(n):
+            for divisor in range(2, int(sqrt(n)) + 1):
+                if n % divisor == 0:
+                    return False
+            return True
+
+        for n in range(2, 100):
+            output = remove_cr_and_lf(remove_junk_line(run_java(class_name, n)))
+
+            expected_output = "-1" if is_prime(n) else "1"
+
+            self.assertEqual(expected_output, output,
+                             "failed on n={}, spl says {}, python says {}".format(n, output, is_prime(n)))
 
 
 if __name__ == "__main__":
